3f2e426bc19c660435a1e1a60057e424b9495233
Yane <36864532+yaneholanda@users.noreply.github.com>
2021-08-23 20:22:47 -0300

    Update queimadas2020.py

--- queimadas2020.py
+++ queimadas2020.py
@@ -5,15 +5,7 @@ import numpy as np
 st.title('Queimadas no Brasil em 2020')
 
 df = pd.read_csv("Focos_2020-01-01_2020-12-31.csv")
-#DATE_COLUMN = df['data']
 df = df.sample(frac=1).reset_index(drop=True)
 
 st.subheader('Map of all pickups')
-hist_values = np.histogram(df["data"].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = df[df["data"].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+st.map(df)
