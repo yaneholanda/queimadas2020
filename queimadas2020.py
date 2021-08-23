a506618322bea2a2560cad73b89f418cd1403d7b
Yane <36864532+yaneholanda@users.noreply.github.com>
2021-08-23 20:19:21 -0300

    Update queimadas2020.py

--- queimadas2020.py
+++ queimadas2020.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('Queimadas no Brasil em 2020')
 
 df = pd.read_csv("Focos_2020-01-01_2020-12-31.csv")
-DATE_COLUMN = df['datahora']
+DATE_COLUMN = df['data']
 df = df.sample(frac=1).reset_index(drop=True)
 
 st.subheader('Map of all pickups')
