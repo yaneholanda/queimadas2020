580ff3777716a4584e085e0c389bab36ff2c60af
Yane <36864532+yaneholanda@users.noreply.github.com>
2021-08-23 21:02:45 -0300

    Update queimadas2020.py

--- queimadas2020.py
+++ queimadas2020.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-df = pd.read_csv("Focos_2020-01-01_2020-12-31.csv")
+df = pd.read_csv("Focos_2020-01-01_2020-12-31.geojson")
 map_data = df
 
 st.map(map_data)
