9f74091c6d2c141209f8c4b9c7b655550124836e
Yane <36864532+yaneholanda@users.noreply.github.com>
2021-08-24 13:16:31 -0300

    Update queimadas2020.py

--- queimadas2020.py
+++ queimadas2020.py
@@ -2,7 +2,28 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-df = pd.read_csv("Focos_2020-01-01_2020-12-31.csv")
-map_data = df
+from streamlit_folium import folium_static
+import folium
 
-st.map(map_data)
+"# streamlit-folium"
+df = pd.read_csv("Focos_2020-01-01_2020-12-31.csv")
+
+lat = df["latitude"]
+long = df["longitude""]
+
+with st.echo():
+    import streamlit as st
+    from streamlit_folium import folium_static
+    import folium
+
+    # center on Liberty Bell
+    m = folium.Map(location=[lat, long], zoom_start=16)
+
+    # add marker for Liberty Bell
+    #tooltip = "Liberty Bell"
+    #folium.Marker(
+    #    [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+    #).add_to(m)
+
+    # call to render Folium map in Streamlit
+    folium_static(m)
